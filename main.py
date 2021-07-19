ff8cffd4f528d2b9713af36c7e37a66c25dde573
MaxFray1 <69054418+MaxFray1@users.noreply.github.com>
2021-07-19 18:52:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from flask import Flask, request
 import telebot
 from telebot import types
 
+import parceInst as inst
+
 TOKEN = '1949484698:AAFkkfN586lV2NjFDu6XBcegj_nN6g7T7q8'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
+inst.init_inst_bot()
+userinst = ""
 
 
 def myKeyboard(count=0, btn1_text="", cb_data1="", btn2_text="", cb_data2=""):
@@ -20,7 +24,7 @@ def myKeyboard(count=0, btn1_text="", cb_data1="", btn2_text="", cb_data2=""):
     return markup
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     text = "Привет это ДзенБот!!! Хочешь получить бесплатный урок?"
     keyboard = myKeyboard(2,'Да', 'Yes1', 'Нет', 'No1')
@@ -29,29 +33,52 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
+    global userinst
+    if(message.text != ""):
+        userinst = message.text
     text = "Хочешь подарок?"
     keyboard = myKeyboard(1,'Да','Yes0')
-    bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=keyboard)
+    print(userinst)
+    # bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback(call):
+    global userinst
+    print(userinst)
     if call.message:
         if call.data == 'Yes1':
             text1 = "Ты подписан на Ваню???"
-            keyboard = myKeyboard(2,'Да', 'Yes2', 'Нет', 'No2')
+            keyboard = myKeyboard(2,'Да', 'Yes3', 'Нет', 'No2')
             bot.delete_message(call.message.chat.id, call.message.id)
             bot.send_message(call.message.chat.id, text1, parse_mode='HTML', reply_markup=keyboard)
         elif call.data == 'No1':
             text1 = "Ты упустил свой шанс заработать миллион рублей, лох"
+            keyboard = myKeyboard(1,'Подарок', 'Yes1')
             bot.delete_message(call.message.chat.id, call.message.id)
-            bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML')
+            bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+        elif call.data == 'Yes3':
+            text1 = "Отправь мне свой ник в инстаграм"
+            keyboard = myKeyboard(1,'Готово', 'Yes2')
+            bot.delete_message(call.message.chat.id, call.message.id)
+            bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
         elif call.data == 'Yes2':
-            text1 = 'Вот твоя ссылка: [http://ссылка.хрю/322qwerty](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
-            bot.delete_message(call.message.chat.id, call.message.id)
-            bot.send_message(call.message.chat.id, text=text1, parse_mode='Markdown')
+            if(userinst != "" and inst.check_follow(userinst)):
+                text1 = 'Вот твоя ссылка: [http://ссылка.хрю/322qwerty](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
+                bot.delete_message(call.message.chat.id, call.message.id)
+                bot.send_message(call.message.chat.id, text=text1, parse_mode='Markdown')
+            else:
+                text1 = "Ты не подписался((( Скорее подпишись на Ваню!!! И пришли свой никнейм" + userinst
+                keyboard = types.InlineKeyboardMarkup(row_width=1)
+                btn1 = types.InlineKeyboardButton('Инста Вани', url='https://www.instagram.com/funtik_iv/')
+                keyboard.add(btn1)
+                btn2 = types.InlineKeyboardButton('Подписался!!!', callback_data='Yes2')
+                keyboard.add(btn2)
+                bot.delete_message(call.message.chat.id, call.message.id)
+                bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+            print(userinst)
         elif call.data == 'No2':
-            text1 = "Скорее подпишись на Ваню!!!"
+            text1 = "Скорее подпишись на Ваню!!! И пришли свой никнейм"
             keyboard = types.InlineKeyboardMarkup(row_width=1)
             btn1 = types.InlineKeyboardButton('Инста Вани', url='https://www.instagram.com/funtik_iv/')
             keyboard.add(btn1)
@@ -64,6 +91,63 @@ def callback(call):
             keyboard = myKeyboard(2,'Да', 'Yes2', 'Нет', 'No2')
             bot.delete_message(call.message.chat.id, call.message.id)
             bot.send_message(call.message.chat.id, text1, parse_mode='HTML', reply_markup=keyboard)
+
+
+
+# def myKeyboard(count=0, btn1_text="", cb_data1="", btn2_text="", cb_data2=""):
+#     markup = types.InlineKeyboardMarkup(row_width=1)
+#     btn1 = types.InlineKeyboardButton(btn1_text, callback_data=cb_data1)
+#     markup.add(btn1)
+#     if(count == 2):
+#         btn2 = types.InlineKeyboardButton(btn2_text, callback_data=cb_data2)
+#         markup.add(btn2)
+#     return markup
+
+
+# @bot.message_handler(commands=['start'])
+# def send_welcome(message):
+#     text = "Привет это ДзенБот!!! Хочешь получить бесплатный урок?"
+#     keyboard = myKeyboard(2,'Да', 'Yes1', 'Нет', 'No1')
+#     bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=keyboard)
+
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     text = "Хочешь подарок?"
+#     keyboard = myKeyboard(1,'Да','Yes0')
+#     bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=keyboard)
+
+
+# @bot.callback_query_handler(func=lambda call:True)
+# def callback(call):
+#     if call.message:
+#         if call.data == 'Yes1':
+#             text1 = "Ты подписан на Ваню???"
+#             keyboard = myKeyboard(2,'Да', 'Yes2', 'Нет', 'No2')
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text1, parse_mode='HTML', reply_markup=keyboard)
+#         elif call.data == 'No1':
+#             text1 = "Ты упустил свой шанс заработать миллион рублей, лох"
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML')
+#         elif call.data == 'Yes2':
+#             text1 = 'Вот твоя ссылка: [http://ссылка.хрю/322qwerty](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text=text1, parse_mode='Markdown')
+#         elif call.data == 'No2':
+#             text1 = "Скорее подпишись на Ваню!!!"
+#             keyboard = types.InlineKeyboardMarkup(row_width=1)
+#             btn1 = types.InlineKeyboardButton('Инста Вани', url='https://www.instagram.com/funtik_iv/')
+#             keyboard.add(btn1)
+#             btn2 = types.InlineKeyboardButton('Подписался!!!', callback_data='Yes2')
+#             keyboard.add(btn2)
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+#         elif call.data == 'Yes0':
+#             text1 = "Ты подписан на Ваню???"
+#             keyboard = myKeyboard(2,'Да', 'Yes2', 'Нет', 'No2')
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text1, parse_mode='HTML', reply_markup=keyboard)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
@@ -83,3 +167,106 @@ def webhook():
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+
+
+
+
+
+
+
+
+
+
+
+# import telebot
+# from telebot import types
+
+# import parceInst as inst
+
+# API_TOKEN = '1920270310:AAEkwYN9_0G_4AnFFbZAODYI-HuOtzOJHPA'
+
+# bot = telebot.TeleBot(API_TOKEN)
+# inst.init_inst_bot()
+# userinst = ""
+
+# def myKeyboard(count=0, btn1_text="", cb_data1="", btn2_text="", cb_data2=""):
+#     markup = types.InlineKeyboardMarkup(row_width=1)
+#     btn1 = types.InlineKeyboardButton(btn1_text, callback_data=cb_data1)
+#     markup.add(btn1)
+#     if(count == 2):
+#         btn2 = types.InlineKeyboardButton(btn2_text, callback_data=cb_data2)
+#         markup.add(btn2)
+#     return markup
+
+
+# @bot.message_handler(commands=['help', 'start'])
+# def send_welcome(message):
+#     text = "Привет это ДзенБот!!! Хочешь получить бесплатный урок?"
+#     keyboard = myKeyboard(2,'Да', 'Yes1', 'Нет', 'No1')
+#     bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=keyboard)
+
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     global userinst
+#     if(message.text != ""):
+#         userinst = message.text
+#     text = "Хочешь подарок?"
+#     keyboard = myKeyboard(1,'Да','Yes0')
+#     print(userinst)
+#     # bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=keyboard)
+
+
+# @bot.callback_query_handler(func=lambda call:True)
+# def callback(call):
+#     global userinst
+#     print(userinst)
+#     if call.message:
+#         if call.data == 'Yes1':
+#             text1 = "Ты подписан на Ваню???"
+#             keyboard = myKeyboard(2,'Да', 'Yes3', 'Нет', 'No2')
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text1, parse_mode='HTML', reply_markup=keyboard)
+#         elif call.data == 'No1':
+#             text1 = "Ты упустил свой шанс заработать миллион рублей, лох"
+#             keyboard = myKeyboard(1,'Подарок', 'Yes1')
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+#         elif call.data == 'Yes3':
+#             text1 = "Отправь мне свой ник в инстаграм"
+#             keyboard = myKeyboard(1,'Готово', 'Yes2')
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+#         elif call.data == 'Yes2':
+#             if(userinst != "" and inst.check_follow(userinst)):
+#                 text1 = 'Вот твоя ссылка: [http://ссылка.хрю/322qwerty](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
+#                 bot.delete_message(call.message.chat.id, call.message.id)
+#                 bot.send_message(call.message.chat.id, text=text1, parse_mode='Markdown')
+#             else:
+#                 text1 = "Ты не подписался((( Скорее подпишись на Ваню!!! И пришли свой никнейм" + userinst
+#                 keyboard = types.InlineKeyboardMarkup(row_width=1)
+#                 btn1 = types.InlineKeyboardButton('Инста Вани', url='https://www.instagram.com/funtik_iv/')
+#                 keyboard.add(btn1)
+#                 btn2 = types.InlineKeyboardButton('Подписался!!!', callback_data='Yes2')
+#                 keyboard.add(btn2)
+#                 bot.delete_message(call.message.chat.id, call.message.id)
+#                 bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+#             print(userinst)
+#         elif call.data == 'No2':
+#             text1 = "Скорее подпишись на Ваню!!! И пришли свой никнейм"
+#             keyboard = types.InlineKeyboardMarkup(row_width=1)
+#             btn1 = types.InlineKeyboardButton('Инста Вани', url='https://www.instagram.com/funtik_iv/')
+#             keyboard.add(btn1)
+#             btn2 = types.InlineKeyboardButton('Подписался!!!', callback_data='Yes2')
+#             keyboard.add(btn2)
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+#         elif call.data == 'Yes0':
+#             text1 = "Ты подписан на Ваню???"
+#             keyboard = myKeyboard(2,'Да', 'Yes2', 'Нет', 'No2')
+#             bot.delete_message(call.message.chat.id, call.message.id)
+#             bot.send_message(call.message.chat.id, text1, parse_mode='HTML', reply_markup=keyboard)
+
+
+# bot.polling()
--- parceInst.py
+++ parceInst.py
@@ -3,18 +3,23 @@ import shutil
 import pathlib
 from instabot import Bot
 
-bot = Bot()
-
 def init_inst_bot():
     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config')
     if pathlib.Path(path).exists():
         shutil.rmtree(path)
+    
+    global bot
+    bot = Bot()
     bot.login(username="_cheiffa_", password="123456789Q!")
 
 def check_follow(username):
+    global bot
     user_id = bot.get_user_id_from_username (username)
     my_followers = bot.get_user_followers('funtik_iv')
     if user_id in my_followers:
         print("FOLLOW")
+        return True
     else:
         print("UNFOLLOW")
+        return False
+
