778c4145c5c427deb2c77ffb22f66319aa97da6c
MaxFray1 <69054418+MaxFray1@users.noreply.github.com>
2021-07-19 16:42:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ from flask import Flask, request
 import telebot
 from telebot import types
 
-import parceInst as inst
-
 TOKEN = '1949484698:AAFkkfN586lV2NjFDu6XBcegj_nN6g7T7q8'
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
-inst.init_inst_bot()
-userinst = ""
+
 
 def myKeyboard(count=0, btn1_text="", cb_data1="", btn2_text="", cb_data2=""):
     markup = types.InlineKeyboardMarkup(row_width=1)
@@ -32,7 +29,6 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    userinst = message.text
     text = "Хочешь подарок?"
     keyboard = myKeyboard(1,'Да','Yes0')
     bot.send_message(message.chat.id, text, parse_mode='HTML', reply_markup=keyboard)
@@ -43,32 +39,17 @@ def callback(call):
     if call.message:
         if call.data == 'Yes1':
             text1 = "Ты подписан на Ваню???"
-            keyboard = myKeyboard(2,'Да', 'Yes3', 'Нет', 'No2')
+            keyboard = myKeyboard(2,'Да', 'Yes2', 'Нет', 'No2')
             bot.delete_message(call.message.chat.id, call.message.id)
             bot.send_message(call.message.chat.id, text1, parse_mode='HTML', reply_markup=keyboard)
         elif call.data == 'No1':
             text1 = "Ты упустил свой шанс заработать миллион рублей, лох"
             bot.delete_message(call.message.chat.id, call.message.id)
             bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML')
-        elif call.data == 'Yes3':
-            text1 = "Отправь мне свой ник в инстаграм"
-            keyboard = myKeyboard(1,'Готово', 'Yes2')
-            bot.delete_message(call.message.chat.id, call.message.id)
-            bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML')
         elif call.data == 'Yes2':
-            if(inst.check_follow(userinst)):
-                text1 = 'Вот твоя ссылка: [http://ссылка.хрю/322qwerty](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
-                bot.delete_message(call.message.chat.id, call.message.id)
-                bot.send_message(call.message.chat.id, text=text1, parse_mode='Markdown')
-            else:
-                text1 = "Ты не подписался((( Скорее подпишись на Ваню!!!"
-                keyboard = types.InlineKeyboardMarkup(row_width=1)
-                btn1 = types.InlineKeyboardButton('Инста Вани', url='https://www.instagram.com/funtik_iv/')
-                keyboard.add(btn1)
-                btn2 = types.InlineKeyboardButton('Подписался!!!', callback_data='Yes2')
-                keyboard.add(btn2)
-                bot.delete_message(call.message.chat.id, call.message.id)
-                bot.send_message(call.message.chat.id, text=text1, parse_mode='HTML', reply_markup=keyboard)
+            text1 = 'Вот твоя ссылка: [http://ссылка.хрю/322qwerty](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
+            bot.delete_message(call.message.chat.id, call.message.id)
+            bot.send_message(call.message.chat.id, text=text1, parse_mode='Markdown')
         elif call.data == 'No2':
             text1 = "Скорее подпишись на Ваню!!!"
             keyboard = types.InlineKeyboardMarkup(row_width=1)
